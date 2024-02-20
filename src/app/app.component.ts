d07dc8db3044ab6533e0dac436e8f927bd5ae685
Armando Quinanga <armando.quinanga@rcsangola.com>
2024-02-20 09:31:02 +0100

    feat: atualizado o projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [MatButtonModule, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular17';
 }
+
+// imports: [RouterOutlet, MatButtonModule]
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: ` <h2 class="bg-green-500">Hello from header</h2>`,
+  styles: ``,
+})
+export class HeaderComponent {}
